5b656e49f58fcdfbb977133e69f3d4ab89acbc88
Tevuz <600871@stud.hvl.no>
2021-10-18 20:50:22 +0200

    Implemented first-fit strategy

--- src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
+++ src/no/hvl/dat100/prosjekt/kontroll/SydSpiller.java
@@ -34,48 +34,24 @@ public class SydSpiller extends Spiller {
 	 * @param topp kort som ligg øverst på til-bunken.
 	 */
 	@Override
-		//#TODO: Imlementer egen AI
 	public Handling nesteHandling(Kort topp) {
-		// ArrayLister for de kort vi har og kan spille
-		Kort[] hand = getHand().getAllekort();
-		KortSamling lovlige = new KortSamling();
-		KortSamling attere = new KortSamling();
 
-		// Gå igjennom kort å finn ut hvilke som kan spilles
-		for (Kort k : hand) {
-			if (Regler.kanLeggeNed(k, topp)) {
-				if (Regler.atter(k)) {
-					attere.leggTil(k);
-				} else {
-					lovlige.leggTil(k);
-				}
-			}
-		}
-
+		//finn første lovlige kort
 		Kort spill = null;
-		Kort[] spillFra = null;
-
-		if (!lovlige.erTom()) {
-			spillFra = lovlige.getAllekort();
-		} else if (!attere.erTom()) {
-			spillFra = attere.getAllekort();
+		for (Kort kort : getHand().getAllekort()){
+			if (Regler.kanLeggeNed(kort, topp)){
+				spill = kort;
+			}
 		}
 
 		Handling handling = null;
 
-		if (spillFra != null) {
-
-			Random r = new Random();
-			int p = r.nextInt(spillFra.length);
-			spill = spillFra[p];
+		if (spill != null) {
 			handling = new Handling(HandlingsType.LEGGNED, spill);
-			// setAntallTrekk(0);
-
 		} else if (getAntallTrekk() < Regler.maksTrekk()) {
 			handling = new Handling(HandlingsType.TREKK, null);
 		} else {
 			handling = new Handling(HandlingsType.FORBI, null);
-			// setAntallTrekk(0);
 		}
 
 		return handling;
